6c3c6c4663a97fa9d2aaa991b8766282a703d112
dommentee <chuku0929@gmail.com>
2021-11-02 09:51:19 -0400

    added files

---
+++ team_project_front/src/App.js
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Video game Stats Tracker</h1>
+    </div>
+  )
+}
+
+export default App;
