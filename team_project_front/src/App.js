8b28e3699ca34db84f5969ba5e008a483e4fd3c4
dommentee <chuku0929@gmail.com>
2021-11-02 11:52:26 -0400

    axios set up with useEffect in App.js

--- team_project_front/src/App.js
+++ team_project_front/src/App.js
@@ -4,10 +4,20 @@ import GameForm from './components/GameForm'
 
 const App = () => {
   let [title, setTitle] = useState()
-  let [genra, setGenra] = useState()
+  let [genre, setGenra] = useState()
   let [completed, setCompleted] = useState(false)
+  let [games, setGames] = useState([])//state of api array //change api array state
 
-  
+  //connects to api and pulls all games 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/games')
+      .then((response) => {
+        setGames(response.data)
+        conole.log(games)
+      })
+  })
+
   return (
     <div className="container">
       <h1>Video game Stats Tracker</h1>
