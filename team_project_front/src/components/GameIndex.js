9cda48196932fff8f1bb4b214c6d9ea5819f0d7f
dommentee <chuku0929@gmail.com>
2021-11-02 15:29:20 -0400

    added game informations

--- team_project_front/src/components/GameIndex.js
+++ team_project_front/src/components/GameIndex.js
@@ -3,14 +3,17 @@ import React from 'react';
 
 
 const GameIndex = (props) => {
-  
+
   return (
     <div>
       <h1>games</h1>
       {props.games.map((game) => {
           return (
-            <div>
-              {game.title}
+            <div className="game_card">
+              <div className="info">Title: {game.title}</div>
+              <div className="info">Genre: {game.genre}</div>
+              <div className="info">Completed: {game.completed}</div>
+              
             </div>
           )
        })}
