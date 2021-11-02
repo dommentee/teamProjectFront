8debfc020d705b91e90a9a333bd4fce7efa8de9d
dommentee <chuku0929@gmail.com>
2021-11-02 11:39:48 -0400

    added front from component

--- team_project_front/src/App.js
+++ team_project_front/src/App.js
@@ -1,10 +1,18 @@
 import React, {useEffect, useState} from 'react';
-import axios from 'axios'
+import axios from 'axios';
+import GameForm from './components/GameForm'
 
 const App = () => {
+  let [title, setTitle] = useState()
+  let [genra, setGenra] = useState()
+  let [completed, setCompleted] = useState(false)
+
+  
   return (
     <div className="container">
       <h1>Video game Stats Tracker</h1>
+      <GameForm />
+
     </div>
   )
 }
---
+++ team_project_front/src/components/GameForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GameForm = () => {
+  return (
+    <div className="form-wrap">
+      <h3>GameForm</h3>
+
+
+    </div>
+  )
+}
+export default GameForm;
